cb15d01772bbed32274764fd9b2745114adcb029
ShadierPond <basm.jewishy120@gmail.com>
2022-08-30 11:09:13 +0200

    GameManager

--- Unity/DerStrand/Assets/Scripts/Management/GameManager.cs
+++ Unity/DerStrand/Assets/Scripts/Management/GameManager.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string[] _persistentScenes = {"Management"};
+    
+    private void UnloadAllNonPersistentScenes()
     {
-        
+        foreach (var scene in SceneManager.GetAllScenes())
+        {
+            if (!(Array.IndexOf(_persistentScenes, scene.name) > -1))
+            {
+                SceneManager.UnloadSceneAsync(scene.name);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadScene(string sceneName)
     {
-        
+        UnloadAllNonPersistentScenes();
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+    }
+
+    private void LoadScene(string[] sceneNames)
+    {
+        UnloadAllNonPersistentScenes();
+        foreach (var sceneName in sceneNames)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        } 
+    }
+
+    private void Start()
+    {
+        LoadScene("Game");
     }
 }
