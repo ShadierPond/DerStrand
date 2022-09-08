f7e925cb30734a766220ac915f993c0a6ecca63d
ShadierPond <basm.jewishy120@gmail.com>
2022-09-08 11:19:14 +0200

    Update ItemDatabase.cs

--- Unity/DerStrand/Assets/Scripts/InventorySystem/Items/Scripts/ItemDatabase.cs
+++ Unity/DerStrand/Assets/Scripts/InventorySystem/Items/Scripts/ItemDatabase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +8,18 @@ public class ItemDatabase : ScriptableObject, ISerializationCallbackReceiver
 {
     // List of all items in the game
     public Item[] items;
-    // Dictionary to get the Item Id from the Item
-    public Dictionary<Item, int> getId = new Dictionary<Item, int>();
     // Dictionary to get the Item from the Item Id
     public Dictionary<int, Item> getItem = new Dictionary<int, Item>();
     public void OnBeforeSerialize()
     {
+        getItem = new Dictionary<int, Item>();
     }
     // When the game starts, add all items to the dictionaries and set their Ids
     public void OnAfterDeserialize()
     {
-        getId = new Dictionary<Item, int>();
-        getItem = new Dictionary<int, Item>();
         for (var i = 0; i < items.Length; i++)
         {
-            getId.Add(items[i], i);
+            items[i].id = i;
             getItem.Add(i, items[i]);
         }
     }
