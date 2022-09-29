150911d7bf020bc085cf92537e3a02b8a6ccb745
ShadierPond <basm.jewishy120@gmail.com>
2022-09-29 09:42:25 +0200

    Update

--- Unity/DerStrand/Assets/Scripts/AIComabt/Target.cs
+++ Unity/DerStrand/Assets/Scripts/AIComabt/Target.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Target : MonoBehaviour
 {
     [SerializeField] float health;              // lifepoints ( for enemy, in inspector adjustable )
+    
+    [SerializeField] Item[] itemsDrops;              // array of items that can be dropped ( in inspector )
+    [SerializeField] int[] itemsDropsAmounts;             // array of dropchances ( in inspector )
+    [SerializeField] float[] dropChance;             // array of dropchances ( in inspector )
+    
 
     public void TakeDamage(float amount)        // claculation of damage ( death )
     {
@@ -18,7 +24,13 @@ public class Target : MonoBehaviour
     void Die()
     {
         Spawner.Instance.enemyGroup.Remove(gameObject);  // clear entry in spawnlist( important for respawn )
+        for (int i = 0; i < itemsDrops.Length; i++)
+        {
+            if(Random.Range(0, 100) <= dropChance[i])
+            {
+                Player.Instance.inventory.AddItem(itemsDrops[i], itemsDropsAmounts[i]);
+            }
+        }
         Destroy(gameObject);                             // destruction of the instanciated object
-
     }
 }
